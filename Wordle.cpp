9fdf26b6263fa6e2aaec82d1db3fccb53772265f
Mars-- .- .- <woodgamesmobile@gmail.com>
2022-04-08 11:28:05 +0200

    Make seperate namespace for Wordle.cpp & Wordle.h

--- Wordle.cpp
+++ Wordle.cpp
@@ -5,7 +5,8 @@
 #include <stdlib.h>
 #include <time.h>
 
-int count_lines(std::fstream& f)
+
+int wordle::count_lines(std::fstream& f)
 {
 	int count = 0;
 	if(f.is_open())
@@ -19,9 +20,9 @@ int count_lines(std::fstream& f)
 	return count;
 }
 
-std::string get_line(std::fstream& f, int l)
+std::string wordle::get_line(std::fstream& f, int l)
 {
-	int length = count_lines(f);
+	int length = wordle::count_lines(f);
 	if( l < 1 || l > length )
 		throw std::length_error("get_line()");
 	
@@ -33,41 +34,41 @@ std::string get_line(std::fstream& f, int l)
 	return result;
 }
 
-std::string get_random_word(std::fstream& f)
+std::string wordle::get_random_word(std::fstream& f)
 {
-	int length = count_lines(f);
+	int length = wordle::count_lines(f);
 	std::srand(time(NULL));
 	int i = std::rand() % length + 1;
 
-	return get_line(f, i);
+	return wordle::get_line(f, i);
 }
 
-bool check_words(std::string correct, std::string toCheck) //!!! I assume the words are of equal length
+bool wordle::check_words(std::string correct, std::string toCheck) //!!! I assume the words are of equal length
 {
 	int l = correct.size();
 
 	for(int j = 0; j < l; j++)
 	{
-		LetterState letterState = LetterState::DontExist;
+		wordle::LetterState letterState = wordle::LetterState::DontExist;
 		
 		for(int i = 0; i < l; i++)
 		{
 			if(correct[i] == toCheck[j] && i == j)
 			{
-				letterState = LetterState::CorrectPlace;
+				letterState = wordle::LetterState::CorrectPlace;
 				break;
 			}	
 			else if(correct[i] == toCheck[j])
 			{
-				letterState = LetterState::IncorrectPlace;
+				letterState = wordle::LetterState::IncorrectPlace;
 			}
 		}	
 
 		switch(letterState)
 		{
-			case LetterState::DontExist: std::cout << "\033[1;37;41m" << toCheck[j];break;
-			case LetterState::CorrectPlace: std::cout << "\033[1;37;42m" << toCheck[j];break;
-			case LetterState::IncorrectPlace: std::cout << "\033[1;37;46m" << toCheck[j];break;
+			case wordle::LetterState::DontExist: std::cout << "\033[1;37;41m" << toCheck[j];break;
+			case wordle::LetterState::CorrectPlace: std::cout << "\033[1;37;42m" << toCheck[j];break;
+			case wordle::LetterState::IncorrectPlace: std::cout << "\033[1;37;46m" << toCheck[j];break;
 		}
 	}
 
@@ -75,7 +76,7 @@ bool check_words(std::string correct, std::string toCheck) //!!! I assume the wo
 	return correct == toCheck;
 }
 
-int search_line_of_word(std::fstream& f, std::string w)
+int wordle::search_line_of_word(std::fstream& f, std::string w)
 {
 	std::string b;
 	int result = 0;
@@ -98,14 +99,14 @@ int search_line_of_word(std::fstream& f, std::string w)
 	return result;
 }
 
-bool word_validation(std::fstream& f, std::string w)
+bool wordle::word_validation(std::fstream& f, std::string w)
 {
 	if(w.size() != 5)
 		return false;
 
 	bool result = false;
 
-	if(search_line_of_word(f, w) > 0)
+	if(wordle::search_line_of_word(f, w) > 0)
 		result = true;
 
 	return result;
--- Wordle.h
+++ Wordle.h
@@ -1,21 +1,22 @@
 #include <fstream>
 #include <string>
 
-
-enum class LetterState
+namespace wordle
 {
-	DontExist, CorrectPlace, IncorrectPlace
-};
+	enum class LetterState
+	{
+		DontExist, CorrectPlace, IncorrectPlace
+	};
 
-int count_lines(std::fstream& f);
+	int count_lines(std::fstream& f);
 
-std::string get_line(std::fstream& f, int l);
+	std::string get_line(std::fstream& f, int l);
 
-std::string get_random_word(std::fstream& f);
+	std::string get_random_word(std::fstream& f);
 
-bool check_words(std::string correct, std::string toCheck); //!!! I assume the words are of equal length
+	bool check_words(std::string correct, std::string toCheck); //!!! I assume the words are of equal length
 
-int search_line_of_word(std::fstream& f, std::string w);
+	int search_line_of_word(std::fstream& f, std::string w);
 
-bool word_validation(std::fstream& f, std::string w);
-
+	bool word_validation(std::fstream& f, std::string w);
+}
